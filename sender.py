002b5880f8b655aae0cc6b6234adb4105455be50
Gabriel Fauth <gabrielfauth@outlook.com>
2022-12-02 17:58:08 -0400

    inserido modelo de mensagem editavel com base no SQL

--- sender.py
+++ sender.py
@@ -2,10 +2,12 @@ import requests as r
 import json
 import fdb
 import time
+from datetime import datetime
 
 class Sender():
     def __init__(self):
         self.config = self.get_config()
+        self.delimiter = self.config['delimiter']
 
     def connDb(self):
         try:
@@ -38,8 +40,7 @@ class Sender():
         '''
         conn = self.connDb()
         try:
-            cur = conn.cursor()
-            cur.execute(sql)
+            conn.execute_immediate(sql)
             conn.commit()
         except:
             input('Atualizou as tabelas?\nAplicativo será fechado!')
@@ -68,50 +69,10 @@ class Sender():
             input('Erro ao localizar CONFIG.JSON\nAplicativo será fechado!\ncod 000')
 
     def get_messages_to_send(self):
-        sql = '''
-        select
-            dime.id as dime_id,
-            vend.parceiro as vendedor_id,
-            rtrim(vend.celular) as vendedor_num,
-            vend.nome as vendedor,
-            pj.projeto_id, pj.dscprojeto,
-            f.id as fase_id, f.fase,
-            pjf.percentual_representacao,
-            t.id as tarefa_id, t.tarefa,
-            pjt.id as projeto_tarefa_id,
-            pjt.percentual_representacao,
-        --    osc.descricao,
-        --    osci.descricao_item as pergunta,
-        --    ptc.resposta,
-            pta.operador as operador_id,
-            ope.nome as operador,
-            pta.responsavel as responsavel_id,
-            resp.nome as responsavel,
-            pta.id as apontamento_id,
-            pta.idn_concluido, 
-            pta.idn_reaberta,
-            pta.enviar
-
-        from projetos pj 
-            left join fvt_dimensionamento dime on dime.id_projeto = pj.projeto_id
-            left join projetos_fases pjf on pjf.id_projeto = pj.projeto_id
-            left join fases f on f.id = pjf.id_fase
-            left join projetos_tarefas pjt on pjt.id_projeto_fase = pjf.id
-            left join tarefas t on t.id = pjt.id_tarefa
-        --    left join tarefas_checklist tc on tc.id_tarefa = t.id
-        --    left join os_check_list osc on osc.check_list = tc.id_checklist
-        --    left join os_check_list_itens osci on osci.check_list = osc.check_list and osci.descricao_item = tc.id_checklist_descricao_item
-        --    left join projetos_tarefas_check ptc on ptc.id_projeto_tarefa = pjt.id and ptc.id_check_list = osc.check_list and ptc.id_check_list_descricao_item = osci.descricao_item
-            left join projetos_tarefas_apontamentos pta on pta.id_projeto_tarefa = pjt.id and pta.idn_concluido = true and pta.idn_reaberta = false
-            left join operadores ope on ope.operador = pta.operador
-            left join parceiros resp on resp.parceiro = pta.responsavel
-            left join parceiros vend on vend.parceiro = dime.vendedor
-
-        where
-            pta.enviar = true and
-            ((pta.enviado = false) or (pta.enviado is null)) and
-            pta.idn_reaberta = false
-        '''
+        try:
+            sql = open('proj_to_send.sql', 'r').read()
+        except:
+            input('Erro ao localizar proj_to_send.sql\nAplicativo será fechado!')
         conn = self.connDb()
         try:
             cur = conn.cursor()
@@ -124,69 +85,25 @@ class Sender():
         finally:
             conn.close()
             return data
-
-    def perc_project(self, dime):
-        sql = f'''
-            select
-                (select
-                    count(pjt.id)
-                
-                from projetos pj 
-                    left join fvt_dimensionamento dime on dime.obra_id = pj.parceiros_obra
-                    left join projetos_fases pjf on pjf.id_projeto = pj.projeto_id
-                    left join fases f on f.id = pjf.id_fase
-                    left join projetos_tarefas pjt on pjt.id_projeto_fase = pjf.id
-                    left join tarefas t on t.id = pjt.id_tarefa
-                
-                where
-                    dime.id = {dime}) as total,
-
-                (select
-                    count(pjt.id)
-                
-                from projetos pj 
-                    left join fvt_dimensionamento dime on dime.obra_id = pj.parceiros_obra
-                    left join projetos_fases pjf on pjf.id_projeto = pj.projeto_id
-                    left join fases f on f.id = pjf.id_fase
-                    left join projetos_tarefas pjt on pjt.id_projeto_fase = pjf.id
-                    left join tarefas t on t.id = pjt.id_tarefa
-                    left join projetos_tarefas_apontamentos pta on pta.id_projeto_tarefa = pjt.id
-                
-                where
-                    dime.id = {dime} and
-                    pta.idn_concluido = true) as concluido
-
-            from (select first 1 * from filiais)
-        '''
-        conn = self.connDb()
-        try:
-            cur = conn.cursor()
-            cur.execute(sql)
-            data = cur.fetchallmap()
-        except:
-            input('Atualizou as tabelas?\nAplicativo será fechado!\ncod 002')
-        finally:
-            conn.close()
-            return data
+    
+    def set_messages(self):
+        messages = []
+        for msg in self.get_messages_to_send():
+            modelo = open('modelo.txt', 'r').read().split(self.delimiter)
+            message = ''
+            for id, m in enumerate(modelo):
+                if id%2 == 1:
+                    print(m)
+                    message += str(msg[m])
+                else:
+                    message += m
+            messages.append({ 'text': message, 'send_to_num': msg['SEND_TO_NUM'], 'apontamento': msg['APONTAMENTO_ID']})
+        return messages
 
     def send_message(self):
-        for mes in self.get_messages_to_send():
-            print(mes)
-            progress = self.perc_project(dime=mes["DIME_ID"])[0]
-            try: 
-                progress = round(int(progress['CONCLUIDO'])/int(progress['TOTAL'])*100, 2)
-            except:
-                progress = 'N/A'
-            message = f"""*STATUS PROJETOS - DELTA SISTEMAS* ```
-Dimen.:   {mes['DIME_ID']}
-Proj.:    {mes['PROJETO_ID']}
-Fase:     {mes['FASE']}
-Tarefa:   {mes['TAREFA']}
-
-Progresso do Projeto:``` *{progress}%*
-"""      
+        for msg in self.set_messages():
+            print(msg)
             
-            print(mes['VENDEDOR_NUM'],mes['VENDEDOR_ID'])
             req = r.post(url=self.config['url'],
                 headers={
                     "Content-Type": "application/json", 
@@ -195,9 +112,9 @@ Progresso do Projeto:``` *{progress}%*
                 },
                 json={
                     "sender_phone": f"{self.config['sender_phone']}",
-                    "phone": f"{mes['VENDEDOR_NUM']}",
+                    "phone": f"{msg['send_to_num']}",
                     "type": "TEXT", 
-                    "text_content": str(message)
+                    "text_content": str(msg['text'])
                 })
             print(req.status_code, req.content)
 
@@ -207,7 +124,7 @@ Progresso do Projeto:``` *{progress}%*
                 cur.execute(f'''
                     UPDATE PROJETOS_TAREFAS_APONTAMENTOS SET
                     ENVIADO = true
-                    WHERE ID = {mes['APONTAMENTO_ID']}
+                    WHERE ID = {msg['apontamento']}
                 ''')
                 conn.commit()
                 conn.close()
@@ -216,4 +133,5 @@ Progresso do Projeto:``` *{progress}%*
 if __name__ == '__main__':
     while True:
         Sender().send_message()
+        # Sender().set_messages()
         time.sleep(10)
